7e3bafb5cd6a89121bd6bc2fead7779dc89618d6
Jahir <jahirgracia05@gmail.com>
2021-12-07 09:58:41 -0700

    Sección 13 terminada - File upload added

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -3,25 +3,27 @@ const { subirArchivo } = require("../helpers");
 const { Usuario, Producto } = require('../models');
 const path = require('path');
 const fs = require('fs');
+const cloudinary = require('cloudinary').v2
+cloudinary.config( process.env.CLOUDINARY_URL);
 
 
-const cargarArchivo = async(req = request, res = response) => {
+const cargarArchivo = async (req = request, res = response) => {
 
     try {
         // txt, md
         // const nombre = await subirArchivo(req.files, ['txt', 'md'], 'textos');
         const nombre = await subirArchivo(req.files, undefined, 'imgs');
         // const nombre = await subirArchivo(req.files, ['txt', 'md'], 'textos');
-    
+
         res.json({
             nombre
         })
     } catch (error) {
-        res.status(400).json({error})
+        res.status(400).json({ error })
     }
 }
 
-const actualizarImagen = async(req = request, res = response) => {
+const actualizarImagen = async (req = request, res = response) => {
 
     const { id, coleccion } = req.params;
 
@@ -29,35 +31,35 @@ const actualizarImagen = async(req = request, res = response) => {
 
     switch (coleccion) {
         case 'usuarios':
-            modelo =  await Usuario.findById(id);
-            if( !modelo ) {
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
                 return res.status(400).json({
-                    msg: `No existe un usuario con el id ${ id }`
+                    msg: `No existe un usuario con el id ${id}`
                 });
             }
 
             break;
 
         case 'productos':
-            modelo =  await Producto.findById(id);
-            if( !modelo ) {
+            modelo = await Producto.findById(id);
+            if (!modelo) {
                 return res.status(400).json({
-                    msg: `No existe un producto con el id ${ id }`
+                    msg: `No existe un producto con el id ${id}`
                 });
             }
 
             break;
-    
-        default: 
-            return res.status(500).json( { msg: 'Se me olvidó validar esto' });
+
+        default:
+            return res.status(500).json({ msg: 'Se me olvidó validar esto' });
     }
 
     // Limpiar imágenes previas
-    if( modelo.img ) {
+    if (modelo.img) {
         // Hay que borrar la imagen del servidor
-        const pathImagen = path.join( __dirname, '../uploads', coleccion, modelo.img );
-        if( fs.existsSync( pathImagen ) ) {
-            fs.unlinkSync( pathImagen ); // <<<--- Sólo si existe en el servidor la eliminamos
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
+        if (fs.existsSync(pathImagen)) {
+            fs.unlinkSync(pathImagen); // <<<--- Sólo si existe en el servidor la eliminamos
         }
     }
 
@@ -69,7 +71,109 @@ const actualizarImagen = async(req = request, res = response) => {
     res.json(modelo);
 }
 
+const actualizarImagenCloudinary = async (req = request, res = response) => {
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`
+                });
+            }
+
+            break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${id}`
+                });
+            }
+
+            break;
+
+        default:
+            return res.status(500).json({ msg: 'Se me olvidó validar esto' });
+    }
+
+    // Limpiar imágenes previas
+    if (modelo.img) {
+        const nombreArr = modelo.img.split('/');
+        const nombre = nombreArr[nombreArr.length -1];
+        const [ public_id ] = nombre.split('.');
+
+        // Lo borramos de cloudinary
+        cloudinary.uploader.destroy(public_id);
+    }
+
+    const { tempFilePath } = req.files.archivo;
+    const { secure_url } = await cloudinary.uploader.upload(tempFilePath);
+    modelo.img = secure_url;
+
+    await modelo.save();
+
+    res.json(modelo);
+}
+
+const mostrarImagen = async (req = request, res = response) => {
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${id}`
+                });
+            }
+
+            break;
+
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${id}`
+                });
+            }
+
+            break;
+
+        default:
+            return res.status(500).json({ msg: 'Se me olvidó validar esto' });
+    }
+
+    // Limpiar imágenes previas
+    if (modelo.img) {
+        // Hay que borrar la imagen del servidor
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
+        if (fs.existsSync(pathImagen)) {
+            return res.sendFile(pathImagen);
+        }
+    } else {
+        const pathImagen = path.join(__dirname, '../assets/no-image.jpg');
+        if (fs.existsSync(pathImagen)) {
+            return res.sendFile(pathImagen);
+        }
+    }
+
+    // res.json({
+    //     msg: 'Falta el place holder'
+    // });
+}
+
 module.exports = {
     cargarArchivo,
-    actualizarImagen
+    actualizarImagen,
+    actualizarImagenCloudinary,
+    mostrarImagen
 }
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { cargarArchivo, actualizarImagen } = require('../controllers/uploads');
+const { cargarArchivo, actualizarImagen, mostrarImagen, actualizarImagenCloudinary } = require('../controllers/uploads');
 const { coleccionesPermitidas } = require('../helpers');
 const { validarCampos, validarArchivoSubir } = require('../middlewares');
 
@@ -14,6 +14,13 @@ router.put('/:coleccion/:id', [
     check('id', 'El id debe de ser de mongo').isMongoId(),
     check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios', 'productos'] ) ),
     validarCampos
-], actualizarImagen);
+// ], actualizarImagen);
+], actualizarImagenCloudinary);
+
+router.get('/:coleccion/:id', [
+    check('id', 'El id debe de ser de mongo').isMongoId(),
+    check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios', 'productos'] ) ),
+    validarCampos
+], mostrarImagen)
 
 module.exports = router;
